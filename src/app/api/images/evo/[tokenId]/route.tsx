78160e21e0ba0e1c7ee025e4e1c3624551803c9b
Nicholas St. Germain <nick@cajun.pro>
2023-10-20 14:43:21 -0500

    temp update for old format

--- src/app/api/images/evo/[tokenId]/route.tsx
+++ src/app/api/images/evo/[tokenId]/route.tsx
@@ -1,48 +1,57 @@
 
 import { EvoCardPng } from "@/components/evo-card/evo-card";
-import { IEvo } from "@/components/evo-card/types";
+import { Gender, IEvo, Nature, Rarity, Species, Type } from "@/components/evo-card/types";
 import { ImageResponse } from 'next/server'
 import { NextRequest } from "next/server";
 
-export const GET = async (req: NextRequest, { params: { tokenId } }: { params: { tokenId: string }}) => {
+export const GET = async (req: NextRequest, { params }: { params: { tokenId: string }}) => {
+  const tokenId = params.tokenId.replace(/[^0-9]/g,  '');
+  console.log(tokenId)
   const resp = await fetch(`https://api.evoverses.com/metadata/evo/${tokenId}`, {
     next: {
-      tags: ["evo", "5"],
-      revalidate: 86400,
+
     }
   })
   const metadata = await resp.json();
-
-  const evo: IEvo = {
+  console.log(metadata)
+  const sizePct = metadata.attributes.find((a: any) => a.trait_type === "Size").value / 10;
+  console.log({
+    total: metadata.attributes.find((a: any) => a.trait_type === "Total Summons").value,
+    remaining: 100,
+    lastBreedTime: metadata.attributes.find((a: any) => a.trait_type === "Last Breed Time").value,
+  })
+  const evo: any = {
     attributes: {
-      gender: metadata.attributes.find((a: any) => a.trait_type === "gender").value,
-      rarity: metadata.attributes.find((a: any) => a.trait_type === "rarity").value,
-      primaryType: metadata.attributes.find((a: any) => a.trait_type === "primaryType").value,
-      secondaryType: metadata.attributes.find((a: any) => a.trait_type === "secondaryType").value,
-      nature: metadata.attributes.find((a: any) => a.trait_type === "nature").value,
-      size: metadata.attributes.find((a: any) => a.trait_type === "size").value,
+      gender: Gender[metadata.attributes.find((a: any) => a.trait_type === "Gender").value],
+      rarity: Rarity[metadata.attributes.find((a: any) => a.trait_type === "Rarity").value],
+      primaryType: Type[metadata.attributes.find((a: any) => a.trait_type === "Primary Type").value],
+      secondaryType: Type[metadata.attributes.find((a: any) => a.trait_type === "Secondary Type")?.value || "None"],
+      nature: Nature[metadata.attributes.find((a: any) => a.trait_type === "Nature").value],
+      size: sizePct > 0 ? 10 + sizePct : sizePct,
     },
     breeds: {
-      total: metadata.attributes.find((a: any) => a.trait_type === "total").value,
+      total: metadata.attributes.find((a: any) => a.trait_type === "Total Summons").value,
       remaining: 100,
-      lastBreedTime: metadata.attributes.find((a: any) => a.trait_type === "lastBreedTime").value,
+      lastBreedTime: metadata.attributes.find((a: any) => a.trait_type === "Last Breed Time").value,
     },
-    experience: metadata.attributes.find((a: any) => a.trait_type === "experience").value,
-    generation: metadata.attributes.find((a: any) => a.trait_type === "generation").value,
+    experience: metadata.attributes.find((a: any) => a.trait_type === "XP").value,
+    generation: metadata.attributes.find((a: any) => a.trait_type === "Generation").value,
     moves: { move0: 0, move1: 0, move2: 0, move3: 0 },
     owner: "",
-    species: metadata.attributes.find((a: any) => a.trait_type === "species").value,
+    species: Species[metadata.attributes.find((a: any) => a.trait_type === "Species").value],
     stats: {
       health: 50,
-      attack: metadata.attributes.find((a: any) => a.trait_type === "attack").value,
-      defense: metadata.attributes.find((a: any) => a.trait_type === "defense").value,
-      special: metadata.attributes.find((a: any) => a.trait_type === "special").value,
-      resistance: metadata.attributes.find((a: any) => a.trait_type === "resistance").value,
-      speed: metadata.attributes.find((a: any) => a.trait_type === "speed").value,
+      attack: metadata.attributes.find((a: any) => a.trait_type === "Attack").value,
+      defense: metadata.attributes.find((a: any) => a.trait_type === "Defense").value,
+      special: metadata.attributes.find((a: any) => a.trait_type === "Special").value,
+      resistance: metadata.attributes.find((a: any) => a.trait_type === "Resistance").value,
+      speed: metadata.attributes.find((a: any) => a.trait_type === "Speed").value,
     },
     tokenId: metadata.tokenId
 
   }
+
+  console.log(evo)
   const sizeMultiplier = 2;
   const baseUrl = req.url.split('/api')[0];
   const nunito = await fetch(
