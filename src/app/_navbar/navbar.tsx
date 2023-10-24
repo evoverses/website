930ade0123c7e36b639c431321471f2ff06c6575
Nicholas St. Germain <nick@cajun.pro>
2023-10-23 23:57:15 -0500

    add logo home link

--- src/app/_navbar/navbar.tsx
+++ src/app/_navbar/navbar.tsx
@@ -3,13 +3,16 @@ import NavItems from "@/app/_navbar/NavItems";
 
 import Image from "next/image";
 import Logo from "@/app/icon.png";
+import Link from "next/link";
 
 const Navbar = () => {
 
   return (
     <div className="border-b fixed w-full bg-background">
       <div className="flex h-16 items-center px-4">
-        <Image src={Logo} alt="EvoVerses" width={48} height={48} />
+        <Link href="/">
+          <Image src={Logo} alt="EvoVerses" width={48} height={48} />
+        </Link>
         <nav className="items-center ml-2 sm:space-x-4 sm:mx-6 lg:space-x-6"> {/* lg:flex  */}
           <NavItems />
 
