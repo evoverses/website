287e04b075eccd48a62c7c4eda4a89809fe3f70d
Nicholas St. Germain <nick@cajun.pro>
2023-11-11 05:49:26 -0600

    remove boba reference and add sharp

--- src/components/providers/web3-provider.tsx
+++ src/components/providers/web3-provider.tsx
@@ -1,15 +1,14 @@
 "use client";
-import { bobaAvax } from "@/lib/constants/wagmi-chains";
 import { EthereumClient, w3mConnectors, w3mProvider } from "@web3modal/ethereum";
 import { Web3Modal } from "@web3modal/react";
+import { useTheme } from "next-themes";
 import { PropsWithChildren } from "react";
-import { publicProvider } from 'wagmi/providers/public'
-import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
 import { Chain, configureChains, createConfig, WagmiConfig } from "wagmi";
 import { avalanche } from "wagmi/chains";
-import { useTheme } from "next-themes";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+import { publicProvider } from "wagmi/providers/public";
 
-const chains = [ avalanche, bobaAvax ];
+const chains = [ avalanche ];
 const projectId = process.env.WALLETCONNECT_PROJECT_ID || process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "";
 
 const { publicClient } = configureChains(chains, [
