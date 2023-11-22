dbf51c99b644f452228816f5fc1df3b581fa2dbf
Nicholas St. Germain <nick@cajun.pro>
2023-11-22 01:45:32 -0600

    add privacy policy redirect

--- next.config.js
+++ next.config.js
@@ -1,22 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    webpackBuildWorker: true,
+  },
   redirects: () => {
     return [
+      { source: "/discord", destination: "https://discord.gg/zxdTHCkpvJ", permanent: false },
+      { source: "/twitter", destination: "https://twitter.com/evoverses", permanent: false },
+      { source: "/github", destination: "https://github.com/evoverses", permanent: false },
       {
-        source: "/discord",
-        destination: "https://discord.gg/zxdTHCkpvJ",
+        source: "/privacy",
+        destination: "https://app.termly.io/document/privacy-policy/836ee923-8be7-4ffb-8bba-e53a9aebe6fe",
         permanent: false,
-      },
-      {
-        source: "/twitter",
-        destination: "https://twitter.com/evoverses",
-        permanent: false,
-      },
-      {
-        source: "/github",
-        destination: "https://github.com/evoverses",
-        permanent: false,
-      },
+      }
     ];
   },
   webpack: (config) => {
