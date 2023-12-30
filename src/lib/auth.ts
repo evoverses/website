47354449130bd1275673054de23434d214cdc74f
Nicholas St. Germain <nick@cajun.pro>
2023-12-29 21:53:48 -0600

    add next public

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,6 @@
 
 export const getCsrfToken = async () => {
-  const url = new URL("/api/auth/csrf", process.env.AUTH_URL);
+  const url = new URL("/api/auth/csrf", process.env.AUTH_URL ?? process.env.NEXT_PUBLIC_AUTH_URL);
   const resp = await fetch(url);
   const data = await resp.json() as { csrfToken: string };
   return data.csrfToken;
