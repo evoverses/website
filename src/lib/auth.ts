c617f72b11272ff3896c36f1f6e0f250a01ae940
Nicholas St. Germain <nick@cajun.pro>
2023-12-29 21:44:35 -0600

    change csrf url

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,6 @@
-const protocol = process.env.NODE_ENV === "production" ? "https" : "http";
-const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL;
+
 export const getCsrfToken = async () => {
-  const url = new URL("/api/auth/csrf", `${protocol}://${process.env.NEXT_PUBLIC_VERCEL_URL}`);
+  const url = new URL("/api/auth/csrf", process.env.AUTH_URL);
   const resp = await fetch(url);
   const data = await resp.json() as { csrfToken: string };
   return data.csrfToken;
