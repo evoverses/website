74fad91e3e71473973873c7c16d0a576d47c84c7
Nicholas St. Germain <nick@cajun.pro>
2023-12-31 02:27:29 -0600

    add success / danger themed classes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,9 +26,17 @@ const config: Config = {
           DEFAULT: "hsl(var(--secondary))",
           foreground: "hsl(var(--secondary-foreground))",
         },
+        success: {
+          DEFAULT: "hsl(var(--success))",
+          foreground: "hsl(var(--success-foreground))",
+        },
         warning: {
           DEFAULT: "hsl(var(--warning))",
           foreground: "hsl(var(--warning-foreground))",
+        },
+        danger: {
+          DEFAULT: "hsl(var(--danger))",
+          foreground: "hsl(var(--danger-foreground))",
         },
         destructive: {
           DEFAULT: "hsl(var(--destructive))",
