889420832d3d412669a722e7e347be62919e3dfa
Nicholas St. Germain <nick@cajun.pro>
2024-01-16 07:44:12 -0600

    update card generation to use tw classes

--- src/app/api/images/evo/[tokenId]/route.tsx
+++ src/app/api/images/evo/[tokenId]/route.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import EvoToken from "@/assets/images/evo-token.png";
 import { getLevelOfEvo } from "@/lib/evoverses/math";
 import { getElementSvg, getGenderImage } from "@/lib/evoverses/svgs";
@@ -55,87 +56,30 @@ export const GET = async (req: NextRequest, { params }: { params: { tokenId: str
     const ageSeconds = Math.floor((
       Date.now() - Date.parse(evo.createdAt)
     ) / 1000);
-    const percentComplete = ageSeconds * 100 / 28800;
+    const pctComplete = ageSeconds * 100 / 28800;
     const ageDays = Math.floor(ageSeconds / 86400);
 
     const baseUrl = req.url.split("/api")[0];
-    const nunito = await fetch(
-      new URL(`/fonts/Nunito.ttf`, baseUrl),
-    ).then((res) => res.arrayBuffer());
-    const nunitoBold = await fetch(
-      new URL(`/fonts/Nunito-Bold.ttf`, baseUrl),
-    ).then((res) => res.arrayBuffer());
-    const nunitoSemiBold = await fetch(
-      new URL(`/fonts/Nunito-SemiBold.ttf`, baseUrl),
-    ).then((res) => res.arrayBuffer());
+
     return new ImageResponse(
       (
-        <div
-          id="container"
-          style={{
-            width: 512,
-            height: 725,
-            borderRadius: 20,
-            position: "relative",
-            display: "flex",
-          }}
-        >
+        <div id="container" tw="w-[512px] h-[725px] rounded-[20px] relative flex text-white font-black text-base">
           <img
             id="background"
             src={`${baseImageApiUrl}/card/background/${isEgg && evo.generation === 0 ? "genesis" : evo.types.primary}`}
-            style={{
-              width: "100%",
-              height: "100%",
-              borderRadius: 30,
-              position: "absolute",
-              top: 0,
-              left: 0,
-            }}
+            tw="w-full h-full rounded-[30px] absolute top-0 left-0"
             alt="background"
           />
           <img
             id="evo"
             src={getEvoUrl(evo)}
-            style={{
-              position: "absolute",
-              width: `400px`,
-              height: `400px`,
-              left: `50%`,
-              transform: `translateX(-50%)`,
-              top: `80px`,
-            }}
+            tw="w-[400px] h-[400px] absolute top-[80px] left-1/2"
+            style={{ transform: `translateX(-50%)` }}
             alt="evo image"
           />
-          <img
-            id="border"
-            style={{ position: "absolute", top: 0, left: 0, bottom: 0, right: 0 }}
-            src={getCardBorder(evo)}
-            alt="border"
-          />
-          <img
-            id="logo"
-            src={`${baseUrl}${EvoToken.src}`}
-            alt="gender"
-            style={{
-              position: "absolute",
-              width: `28px`,
-              height: `28px`,
-              top: `10px`,
-              left: `10px`,
-            }}
-          />
-          <p
-            id="species"
-            style={{
-              fontFamily: "Nunito",
-              color: "#FFFFFF",
-              fontWeight: "900",
-              fontSize: `16px`,
-              position: "absolute",
-              top: `35px`,
-              left: `32px`,
-            }}
-          >
+          <img id="border" tw="absolute w-full h-full" src={getCardBorder(evo)} alt="border" />
+          <img id="logo" src={`${baseUrl}${EvoToken.src}`} alt="gender" tw="w-7 h-7 absolute top-2.5 left-2.5" />
+          <p id="species" tw="absolute top-8 left-8">
             {isEgg && evo.generation === 0 ? "Genesis Unknown" : evo.species}
           </p>
           {[ evo.types.primary, evo.types.secondary || "None" ]
@@ -147,149 +91,48 @@ export const GET = async (req: NextRequest, { params }: { params: { tokenId: str
                 id={`type-${t}`}
                 src={`${baseUrl}${getElementSvg(t).src}`}
                 alt="Species"
-                style={{
-                  position: "absolute",
-                  display: "flex",
-                  width: `18px`,
-                  height: `18px`,
-                  top: `54px`,
-                  left: a.length === 1 ? `155px` : `${i === 0 ? 162 : 148}px`,
-                }}
+                tw="absolute flex w-[18px] h-[18px] absolute top-[54px]"
+                style={{ left: a.length === 1 ? `155px` : `${i === 0 ? 162 : 148}px` }}
               />
             ))}
           {!isEgg && (
             <img
               src={`${baseUrl}${getGenderImage(evo.gender)}`}
               alt="gender"
-              style={{
-                position: "absolute",
-                width: `16px`,
-                height: `16px`,
-                top: `56px`,
-                left: `120px`,
-              }}
+              tw="w-4 h-4 absolute top-[56px] left-[120px]"
             />
           )}
-          <p
-            id="level-info"
-            style={{
-              fontFamily: "Nunito",
-              color: "#FFFFFF",
-              fontWeight: "900",
-              fontSize: `16px`,
-              position: "absolute",
-              top: `85px`,
-              left: `32px`,
-            }}
-          >
+          <p id="level-info" tw="absolute top-[82px] left-[32px]">
             {isEgg ? "Egg" : `Level ${getLevelOfEvo(evo)}`}
           </p>
           <img
             id="info-overlay"
-            style={{
-              position: "absolute",
-              width: `512px`,
-              height: `725px`,
-              top: `60px`,
-              opacity: 0.8,
-              filter: "invert(100%)",
-            }}
+            tw="absolute w-full h-full top-[60px] opacity-80"
+            style={{ filter: "invert(100%)" }}
             src={`${baseImageApiUrl}/card/overlay/info-island`}
             alt="info-overlay-bg"
           />
           <p
-            style={{
-              fontFamily: "Nunito",
-              fontWeight: 900,
-              color: "#FFFFFF",
-              position: "absolute",
-              fontSize: `16px`,
-              left: "50%",
-              transform: "translateX(-50%)",
-              bottom: `215px`,
-            }}
+            tw="absolute left-1/2 bottom-[212px]"
+            style={{ transform: "translateX(-50%)" }}
           >
             {isEgg ? `Age: ${ageDays} Day${ageDays === 1 ? "" : "s"}` : evo.nature}
           </p>
           {isEgg ? (
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center",
-                width: "100%",
-                maxWidth: `300px`,
-                marginLeft: "auto",
-                marginRight: "auto",
-                top: `65px`,
-              }}
-            >
-              <p
-                style={{
-                  textAlign: "center",
-                  fontFamily: "Nunito",
-                  fontWeight: 900,
-                  fontSize: `16px`,
-                  color: "#FFFFFF",
-                  position: "absolute",
-                  bottom: `230px`,
-                  left: `50%`,
-                  transform: `translateX(-50%)`,
-                }}
-              >
+            <div tw="flex flex-col justify-center items-center w-full max-w-[300px] mx-auto top-[65px]">
+              <p tw="absolute left-1/2 bottom-[230px]" style={{ transform: `translateX(-50%)` }}>
                 {evo.generation > 0 ? <>Parents: {evo.parents.map(n => `#${n.tokenId}`).join(" & ")}</> : "Genesis"}
               </p>
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "row",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  width: "100%",
-                  maxWidth: `300px`,
-                  marginLeft: "auto",
-                  marginRight: "auto",
-                  top: `135px`,
-                }}
-              >
-                <div
-                  style={{
-                    backgroundColor: "rgba(255, 255, 255, 1)",
-                    width: "100%",
-                    marginLeft: "16px",
-                    marginRight: "16px",
-                    height: "4px",
-                    borderRadius: "8px",
-                    overflow: "hidden",
-                    display: "flex",
-                  }}
-                >
-                  <div
-                    style={{
-                      backgroundColor: "#51FFFE",
-                      width: `${percentComplete > 100 ? 100 : percentComplete}%`,
-                      height: "100%",
-                    }}
-                  />
+              <div tw="flex justify-center items-center w-full max-w-[300px] mx-auto top-[135px]">
+                <div tw="flex h-1 mx-4 w-full rounded-lg overflow-hidden bg-white">
+                  <div tw="bg-[#51FFFE] h-full" style={{ width: `${pctComplete > 100 ? 100 : pctComplete}%` }} />
                 </div>
               </div>
             </div>
-
           ) : (
             <div
-              style={{
-                width: "350px",
-                height: `64px`,
-                position: "absolute",
-                bottom: "148px",
-                left: "50%",
-                transform: "translateX(-50%)",
-                paddingLeft: `8px`,
-                display: "flex",
-                justifyItems: "center",
-                flexWrap: "wrap",
-              }}
+              tw="w-[350px] h-[64px] absolute bottom-[148px] pl-2 left-1/2 flex items-center flex-wrap"
+              style={{ transform: "translateX(-50%)" }}
             >
               <GenItem stat="HP" value={50} />
               <GenItem stat="ATK" value={evo.attack} />
@@ -299,63 +142,17 @@ export const GET = async (req: NextRequest, { params }: { params: { tokenId: str
               <GenItem stat="SPD" value={evo.speed} />
             </div>
           )}
-          <p
-            style={{
-              fontFamily: "Nunito",
-              fontWeight: 900,
-              position: "absolute",
-              fontSize: `16px`,
-              color: "#FFFFFF",
-              left: `50%`,
-              transform: `translateX(-50%)`,
-              bottom: `88px`,
-            }}
-          >
+          <p tw="absolute left-1/2 bottom-[88px]" style={{ transform: `translateX(-50%)` }}>
             {isEgg ? evo.treated ? "Treated" : "Untreated" : `${evo.generation === 0
               ? "Total Breeds"
               : "Breeds Left"}: ${evo.generation === 0 ? evo.totalBreeds : 5 - evo.totalBreeds}`}
           </p>
-          <p
-            style={{
-              fontFamily: "Nunito",
-              color: "#000000",
-              fontWeight: "900",
-              fontSize: `16px`,
-              position: "absolute",
-              bottom: `83px`,
-              right: `32px`,
-              display: "flex",
-            }}
-          >
-            #{evo.tokenId}
-          </p>
-          <p
-            style={{
-              fontFamily: "Nunito",
-              color: "#000000",
-              fontWeight: "900",
-              fontSize: `16px`,
-              position: "absolute",
-              bottom: `37px`,
-              right: `32px`,
-              display: "flex",
-            }}
-          >
-            Generation {evo.generation}
-          </p>
+          <p tw="text-black absolute flex right-8 bottom-[81px]">#{evo.tokenId}</p>
+          <p tw="text-black absolute flex right-8 bottom-[35px]">Generation {evo.generation}</p>
           {evo.types.primary && evo.types.primary !== "None" && (
             <img
               src={`${baseUrl}${getElementSvg(evo.types.primary).src}`}
-              style={{
-                position: "absolute",
-                width: `28px`,
-                height: `28px`,
-                backgroundPosition: "center",
-                backgroundRepeat: "no-repeat",
-                backgroundSize: "contain",
-                bottom: `10px`,
-                right: `10px`,
-              }}
+              tw="absolute w-7 h-7 bottom-2.5 right-2.5"
               alt="type"
             />
           )}
@@ -365,13 +162,15 @@ export const GET = async (req: NextRequest, { params }: { params: { tokenId: str
         width: 512,
         height: 725,
         fonts: [
-          { name: "Nunito", data: nunito, weight: 400, style: "normal" },
-          { name: "Nunito", data: nunitoSemiBold, weight: 500, style: "normal" },
-          { name: "Nunito", data: nunitoBold, weight: 900, style: "normal" },
+          {
+            name: "Nunito",
+            data: await fetch(new URL(`/fonts/Nunito-Bold.ttf`, baseUrl)).then((res) => res.arrayBuffer()),
+            weight: 900,
+            style: "normal",
+          },
         ],
       },
-    )
-      ;
+    );
   } catch (e: any) {
     console.log(`${e.message}`);
     return new Response(`Failed to generate the image`, { status: 500 });
@@ -381,41 +180,10 @@ export const GET = async (req: NextRequest, { params }: { params: { tokenId: str
 
 const GenItem = ({ stat, value }: { stat: StatAbbrev, value: number }) => {
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "32px",
-        width: "110px",
-      }}
-    >
-      <p
-        style={{
-          textAlign: "right",
-          fontFamily: "Nunito",
-          fontWeight: 900,
-          fontSize: `16px`,
-          color: "#FFF",
-          margin: 0,
-          padding: 0,
-        }}
-      >
-        {stat}:
-      </p>
-      <p
-        style={{
-          textAlign: "right",
-          fontFamily: "Nunito",
-          fontWeight: 900,
-          fontSize: `16px`,
-          margin: 0,
-          marginLeft: "4px",
-          padding: 0,
-        }}
-      >
-        <span style={{ color: `rgb(255, 215, 0)` }}>{value}</span>
+    <div tw="flex justify-center h-8 w-[110px] items-center">
+      <p tw="text-right m-0 p-0">{stat}:</p>
+      <p tw="text-right m-0 ml-1 p-0">
+        <span tw="text-[#ffd700]">{value}</span>
       </p>
     </div>
   );
