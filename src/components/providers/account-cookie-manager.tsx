82d7ce7f0a543dd0325d0b6a3e9d03676518a7ef
Nicholas St. Germain <nick@cajun.pro>
2024-04-18 12:02:33 -0500

    add refresh to stop first login spam

--- src/components/providers/account-cookie-manager.tsx
+++ src/components/providers/account-cookie-manager.tsx
@@ -3,18 +3,23 @@
 import { DeadBeef } from "@/data/constants";
 import { setAccountCookieAddressAction } from "@/lib/cookies/account.actions";
 import type { IAccountCookie } from "@/types/cookies";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { useAccount } from "wagmi";
 
 const AccountCookieManager = ({ accountCookie: { address: cookieAddress } }: { accountCookie: IAccountCookie }) => {
+  const router = useRouter();
   const { address = DeadBeef } = useAccount();
   const [ isSetting, setIsSetting ] = useState<boolean>(false);
   useEffect(() => {
     if (String(cookieAddress).toLowerCase() !== String(address).toLowerCase() && !isSetting) {
-      console.debug("Updating Account address cookie from", cookieAddress, "to", address);
       setIsSetting(true);
-      setAccountCookieAddressAction(address).then(() => setIsSetting(false));
+      setAccountCookieAddressAction(address).then(() => {
+        setIsSetting(false);
+        router.refresh();
+      });
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [ address, cookieAddress, isSetting ]);
 
   return null;
