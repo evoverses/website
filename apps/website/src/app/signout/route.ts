1133b8522288dc7859713b9864c6233c549f490b
Nicholas St. Germain <nick@cajun.pro>
2025-06-24 19:25:11 -0500

    update pnpm
    Signed-off-by: Nicholas St. Germain <nick@cajun.pro>

--- apps/website/src/app/signout/route.ts
+++ apps/website/src/app/signout/route.ts
@@ -1,5 +1,10 @@
 import { logout } from "@/lib/thirdweb/auth";
+import { type NextRequest, NextResponse } from "next/server";
 
-export const GET = async () => {
-   await logout();
+export const GET = async (request: NextRequest) => {
+  await logout();
+  const url = request.nextUrl.clone();
+  url.searchParams.forEach((value, key) => url.searchParams.delete(key));
+  url.pathname = "/";
+  return NextResponse.redirect(url.toString());
 };
